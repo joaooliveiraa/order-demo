f39443ec55e1beb4fa036c751931c46da211c8d3
João Oliveira <68453333+joaooliveiraa@users.noreply.github.com>
2025-01-26 20:21:26 -0300

    Update PedidoService.java

--- demo/src/main/java/com/example/demo/service/PedidoService.java
+++ demo/src/main/java/com/example/demo/service/PedidoService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.PedidoDto;
 import com.example.demo.entity.Pedido;
 import com.example.demo.entity.Produto;
 import com.example.demo.repository.PedidoRepository;
@@ -10,6 +11,8 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class PedidoService {
@@ -22,11 +25,15 @@ public class PedidoService {
 
     private static final String TOPIC = "topico-pedido";
 
-    public Pedido processarPedido(Pedido pedido) {
-        BigDecimal valorTotal = pedido.getProdutos().stream()
+    public Pedido processarPedido(PedidoDto pedidoDto) {
+
+        Pedido pedido = new Pedido();
+
+        BigDecimal valorTotal = pedidoDto.getProdutos().stream()
                 .map(Produto::getPreco)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
 
+        pedido.setCliente(pedidoDto.getCliente());
         pedido.setValorTotal(valorTotal);
         pedido.setStatus("Finalizado");
 
@@ -34,19 +41,37 @@ public class PedidoService {
         return pedido;
     }
 
-    public void enviarParaKafka(Pedido pedido) {
-        String pedidoJson = "";
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            pedidoJson = objectMapper.writeValueAsString(pedido);
-        } catch (Exception e) {
-
+    public void enviarParaKafka(PedidoDto pedidoDto) {
+        String pedidoJson = serializarPedido(pedidoDto);
+        if (pedidoJson != null) {
+            kafkaTemplate.send(TOPIC, pedidoJson);
+            System.out.println("Pedido enviado com sucesso - Status: " + pedidoDto.getStatus());
+        } else {
+            System.out.println("Falha ao serializar o pedido. Pedido não enviado.");
         }
-        System.out.println("Pedido enviado " + pedido.getStatus());
-        kafkaTemplate.send(TOPIC, pedidoJson);
     }
 
-    public Pedido consultarPedido(Long id) {
-        return pedidoRepository.findById(id).orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+    private String serializarPedido(PedidoDto pedidoDto) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(pedidoDto);
+        } catch (JsonProcessingException e) {
+            System.out.println("Erro ao serializar PedidoDto para JSON: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public List<Pedido> consultarPedidos(Long id, String status) {
+        if (id != null) {
+            return pedidoRepository.findById(id)
+                    .map(Collections::singletonList)
+                    .orElse(Collections.emptyList());
+        }
+
+        if (status != null && status.equalsIgnoreCase("finalizado")) {
+            return pedidoRepository.findByStatus("Finalizado");
+        }
+
+        return pedidoRepository.findAll();
     }
 }
