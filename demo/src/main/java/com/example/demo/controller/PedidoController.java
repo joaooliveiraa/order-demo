a1d9b8b8edf389fb35e83d333842bb95529514fc
João Oliveira <68453333+joaooliveiraa@users.noreply.github.com>
2025-01-26 20:07:47 -0300

    Update PedidoController.java

--- demo/src/main/java/com/example/demo/controller/PedidoController.java
+++ demo/src/main/java/com/example/demo/controller/PedidoController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.PedidoDto;
 import com.example.demo.entity.Pedido;
 import com.example.demo.producer.PedidoProducer;
 import com.example.demo.service.PedidoService;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -19,14 +22,17 @@ public class PedidoController {
     private PedidoProducer pedidoProducer;
 
     @PostMapping
-    public ResponseEntity<Void> criarPedido(@RequestBody Pedido pedido) {
-        pedidoService.enviarParaKafka(pedido);   //Envia para Kafka para processamento assíncrono
+    public ResponseEntity<Void> criarPedido(@RequestBody PedidoDto pedidoDto) {
+        pedidoService.enviarParaKafka(pedidoDto);   //Envia para Kafka para processamento assíncrono
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Pedido> consultarPedido(@PathVariable Long id) {
-        Pedido pedido = pedidoService.consultarPedido(id);  // Consulta do pedido após cálculo
-        return ResponseEntity.ok(pedido);
+    @GetMapping
+    public ResponseEntity<List<Pedido>> consultarPedidos(
+            @RequestParam(value = "id", required = false) Long id,
+            @RequestParam(value = "status", required = false) String status) {
+        List<Pedido> pedidos = pedidoService.consultarPedidos(id, status);
+        return ResponseEntity.ok(pedidos);
     }
+
 }
