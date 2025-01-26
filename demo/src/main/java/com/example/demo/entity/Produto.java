1386b50df668fbca23599b1d6a3da74c35bc9f00
João Oliveira <68453333+joaooliveiraa@users.noreply.github.com>
2025-01-26 20:20:27 -0300

    Update Produto.java

--- demo/src/main/java/com/example/demo/entity/Produto.java
+++ demo/src/main/java/com/example/demo/entity/Produto.java
@@ -16,27 +16,9 @@ public class Produto {
     private String nome;
     private BigDecimal preco;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     public BigDecimal getPreco() {
         return preco;
     }
 
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
 }
